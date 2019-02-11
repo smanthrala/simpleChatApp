08bfe96e85a0d1d2a0632d2bda98229d049d978c
smanthrala <smanthra@gmail.com>
2019-02-11 01:56:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5000
 
 
 //set the template engine ejs
@@ -15,7 +16,7 @@ app.get('/', (req, res) => {
 })
 
 //Listen on port 3000
-server = app.listen(5000)
+server = app.listen(port)
 
 
 
